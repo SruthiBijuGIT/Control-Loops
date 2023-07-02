6e7afb47900e32cea873f8527ebf531d90a29ee4
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-07-02 09:36:43 -0700

    Create Eligible for voting.py

---
+++ Eligible for voting.py
@@ -0,0 +1,7 @@
+#Implement python program to read person's age from keyboard and display whether he is eligible for voing or not
+x=int(input())
+if x>=18:
+  print("Eligible for Voting")
+else:
+  print("Not Eligible for Voting")
+
