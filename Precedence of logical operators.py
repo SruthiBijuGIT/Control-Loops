bd3edfb06acaa8ca5192e066daee54f985371502
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-07-02 09:47:20 -0700

    Create Precedence of logical operators.py

---
+++ Precedence of logical operators.py
@@ -0,0 +1,16 @@
+# Write a Python program to perform the following logical operations. Give Precedence order of logical operators. 
+#Given x=True, y=False
+# A. x and not y
+# B. not x and y 
+# C. not x and not y 
+# D. x or not y 
+# E. not x or y 
+# F. not x or not y 
+x= True
+y= False
+print(x and not y)
+print(not x and y )
+print(not x and not y )
+print( x or not y )
+print(not x or y )
+print(not x or not y) 
